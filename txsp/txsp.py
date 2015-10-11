111de75f51fca21c761691a218f85f55400c1525
lvqier <lvqier@gmail.com>
2015-10-11 13:27:45 +0800

    Update crawler for v.qq.com, update to 2015-10-10

--- txsp/txsp.py
+++ txsp/txsp.py
@@ -9,14 +9,17 @@ import re
 import sys
 import time
 import urllib
+import urllib2
 import uuid
 
-from qqtea import ccc
+from echo_ckeyv3 import echo_ckeyv3
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0'
-SWF_REFERER = 'http://imgcache.qq.com/tencentvideo_v1/player/TencentPlayer.swf?max_age=86400&v=20140917'
-PLAYER_PLATFORM = 11
-PLAYER_VERSION = '3.2.18.285'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:41.0) Gecko/20100101 Firefox/41.0'
+SWF_REFERER = 'http://imgcache.qq.com/tencentvideo_v1/player/TencentPlayer.swf?max_age=86400&v=20151010'
+PLATFORM = 11
+PLAYER_GUID = uuid.uuid4().hex
+PLAYER_PID = uuid.uuid4().hex
+PLAYER_VERSION = '3.2.19.336'
 KLIB_VERSION = '2.0'
 
 def get_url(browser, page_url, working_dir=None):
@@ -39,42 +42,143 @@ def to_dict(json_object):
             return key
     return eval(json_object, global_dict())
 
-def load_key(browser):
-    url = 'http://vv.video.qq.com/checktime'
-    resp = browser.open(url)
-    xml = etree.fromstring(resp.read())
-    t = int(xml.xpath('/root/t/text()')[0])
+def getvinfo(target_dir, url, vid):
+    rand = random.random()
+    ckey = echo_ckeyv3(vid, PLAYER_GUID, rand, player_version=PLAYER_VERSION, platform=PLATFORM)
+    params = {
+        'fp2p': 1,
+        'charge': 0,
+        'guid': PLAYER_GUID,
+        'pid': PLAYER_PID,
+        'vid': vid,
+        'platform': PLATFORM,
+        'cKey': ckey,
+        'otype': 'xml', 
+        'fhdswitch': 0,
+        'defnpayver': 1,
+        'appver': PLAYER_VERSION,
+        'ehost': url, 
+        'vids': vid,
+        'encryptVer': '5.4',
+        'utype': 0,
+        'ran': '%.16f' % rand,
+        'speed': random.randint(1000, 3000)
+    }
+    request = urllib2.Request('http://vv.video.qq.com/getvinfo')
+    request.add_header('Referer', SWF_REFERER)
+    request.add_header('Content-type', 'application/x-www-form-urlencoded')
+    request.add_header('User-Agent', USER_AGENT)
+    
+    form = urllib.urlencode(params)
+    resp = urllib2.urlopen(request, data=form)
 
-    return ccc(PLAYER_PLATFORM, PLAYER_VERSION, t)
+    vinfo = resp.read()
+    tree = etree.fromstring(vinfo)
 
-def get_user_agent(vid, fmt):
-    ua = 'Mozilla/5.0 TencentPlayerVod_1.1.91 tencent_-%s-%s' % (vid, fmt)
-    return ua
+    resolutions = {}
+    slid = None
+    for fi in tree.xpath('/root/fl/fi'):
+        name = fi.xpath('name/text()')[0]
+        fiid = int(fi.xpath('id/text()')[0])
+        sl = int(fi.xpath('sl/text()')[0])
+        cname = fi.xpath('cname/text()')[0]
+        if sl:
+            slid = fiid
+            print 'Selected %s: %s' % (name, cname)
+        resolutions[name] = fiid
 
-def get_from(url):
-    return 'v1001'
+    print resolutions
 
-def download_clip(target_file, size, user_agent, url, fmt_name, type_name, br, sp, vkey, level):
-    browser = Browser()
-    browser.set_handle_robots(False)
+    for vi in tree.xpath('/root/vl/vi'):
+        video_type = int(vi.xpath('videotype/text()')[0])
+        if video_type == 1:
+            video_type = 'flv'
+        elif video_type == 2:
+            video_type = 'mp4'
+        else:
+            video_type = 'unknown'
 
-    form = {
-        'stdfrom': get_from(url),
-        'type': type_name,
-        'vkey': vkey,
-        'level': level,
-        'platform': PLAYER_PLATFORM,
-        'br': br,
-        'fmt': fmt_name,
-        'sp': sp,
+        video_id = vi.xpath('vid/text()')[0]
+
+        cdn_host = vi.xpath('ul/ui/url/text()')[0]
+        vt = vi.xpath('ul/ui/vt/text()')[0]
+
+        for ci in vi.xpath('cl/ci'):
+            idx = int(ci.xpath('idx/text()')[0])
+            cd = float(ci.xpath('cd/text()')[0])
+
+            vclip = getvclip(url, vid, vt, slid, idx)
+
+            filename = vclip['filename']
+            key = vclip['key']
+
+            print '%d: %s (%f, %d) %s' % (idx, filename, cd, vclip['fs'], vclip['md5'])
+
+            cdn_url = '%s/%s' % (cdn_host, filename)
+            download_vclip(os.path.join(target_dir, filename), cdn_url, key, vclip['br'], vclip['fmt'], vclip['fs'])
+
+
+def getvclip(url, vid, vt, resolution, idx):
+    rand = random.random()
+    ckey = echo_ckeyv3(vid, PLAYER_GUID, rand, player_version=PLAYER_VERSION, platform=PLATFORM)
+    params = {
+        'buffer': 0,
+        'guid': PLAYER_GUID,
+        'vt': vt,
+        'ltime': 77,
+        'fmt': 'auto',
+        'vid': vid,
+        'platform': PLATFORM,
+        'cKey': ckey,
+        'format': resolution,
+        'speed': random.randint(1000, 3000),
+        'encryptVer': '5.4',
+        'idx': idx,
+        'appver': PLAYER_VERSION,
+        'ehost': url,
+        'dltype': 1,
+        'charge': 0,
+        'otype': 'xml',
+        'ran': '%.16f' % rand
     }
 
-    query_string = urllib.urlencode(form)
-    browser.addheaers = [
-        ('User-Agent', user_agent),
-        ('x-flash-version', 'MAC 15,0,0,189')]
+    request = urllib2.Request('http://vv.video.qq.com/getvclip')
+    request.add_header('Referer', SWF_REFERER)
+    request.add_header('Content-type', 'application/x-www-form-urlencoded')
+    request.add_header('User-Agent', USER_AGENT)
+    
+    form = urllib.urlencode(params)
+    resp = urllib2.urlopen(request, data=form)
 
-    resp = browser.open('%s?%s' % (url, query_string))
+    vclip = resp.read()
+    tree = etree.fromstring(vclip)
+
+    vclip = {
+        'filename': tree.xpath('/root/vi/fn/text()')[0],
+        'br': float(tree.xpath('/root/vi/br/text()')[0]),
+        'fmt': tree.xpath('/root/vi/fmt/text()')[0],
+        'key': tree.xpath('/root/vi/key/text()')[0],
+        'md5': tree.xpath('/root/vi/md5/text()')[0],
+        'fs': int(tree.xpath('/root/vi/fs/text()')[0])
+    }
+
+    return vclip
+
+
+def download_vclip(target_file, url, vkey, br, fmt, size, sp=0):
+    params = {
+        'sdtfrom': 'v1000',
+        'type': 'mp4',
+        'vkey': vkey,
+        'platform': PLATFORM,
+        'br': br,
+        'fmt': fmt,
+        'sp': sp,
+        'guid': PLAYER_GUID
+    }
+
+    url = '%s?%s' % (url, urllib.urlencode(params))
+    resp = urllib2.urlopen(url)
 
     start_time = time.time()
     downloaded_size = 0
@@ -88,7 +192,7 @@ def download_clip(target_file, size, user_agent, url, fmt_name, type_name, br, s
         downloaded_size += len(data)
         speed = (len(data)/1024)/(time.time() - st)
         percent = (downloaded_size * 100)/size
-        print '[%%%d] %dKB/s %d/%d\r' % (percent, speed, downloaded_size, size),
+        print '[%d%%] %dKB/s %d/%d\r' % (percent, speed, downloaded_size, size),
         sys.stdout.flush()
 
     time_spent = time.time() - start_time
@@ -98,121 +202,11 @@ def download_clip(target_file, size, user_agent, url, fmt_name, type_name, br, s
 
     of.close()
 
-def get_videoinfo(browser, target_dir, vid):
-    browser.addheaders = [('User-Agent', USER_AGENT), ('Referer', SWF_REFERER)]
-    player_pid = uuid.uuid4().hex.upper()
-    params = {
-        'vids': vid,
-        'vid': vid,
-        'otype': 'xml',
-        'defnpayver': 1,
-        'platform': PLAYER_PLATFORM,
-        'charge': 0,
-        'ran': random.random(),
-        'speed': 8096, #random.randint(2048, 8096),
-        'pid': player_pid,
-        'appver': PLAYER_VERSION,
-        'fhdswitch': 0,
-        'defn': 'shd',  # default to super hd
-        'defaultfmt': 'shd', # default to super hd
-        'fp2p': 1,
-        'utype': 0,
-        'cKey': load_key(browser),
-        'encryptVer': KLIB_VERSION,
-    }
-
-    form = urllib.urlencode(params)
-    resp = browser.open('http://vv.video.qq.com/getvinfo', data=form)
-    vinfo = resp.read()
-    # print vinfo
-    # open('a.xml', 'wb').write(vinfo)
-    tree = etree.fromstring(vinfo)
-
-    fmt_id = None
-    fmt_name = None
-    fmt_br = None
-    for fmt in tree.xpath('/root/fl/fi'):
-        sl = int(fmt.xpath('sl/text()')[0])
-        if sl:
-#        name = fmt.xpath('name/text()')[0]
-#        if name == 'shd':
-            fmt_id = fmt.xpath('id/text()')[0]
-            fmt_name = fmt.xpath('name/text()')[0]
-            fmt_br = fmt.xpath('br/text()')[0]
-
-    assert fmt_id
-
-    video = tree.xpath('/root/vl/vi')[0]
-    filename = video.xpath('fn/text()')[0]
-    filesize = video.xpath('fs/text()')[0]
-
-    cdn = video.xpath('ul/ui')[0]
-    cdn_url = cdn.xpath('url/text()')[0]
-    filetype = int(cdn.xpath('dt/text()')[0])
-    vt = cdn.xpath('vt/text()')[0]
-
-    if filetype == 1:
-        type_name = 'flv'
-    elif filetype == 2:
-        type_name = 'mp4'
-    else:
-        type_name = 'unknown'
-
-    clips = []
-    for ci in video.xpath('cl/ci'):
-        clip_size = int(ci.xpath('cs/text()')[0])
-        clip_idx = int(ci.xpath('idx/text()')[0])
-        clips.append({'idx': clip_idx, 'size': clip_size})
-
-    print 'File name:', filename
-    print 'Size: %s (%s) in %d clips:' % (filesize, fmt_name, len(clips)),
-    for clip in clips:
-        print clip['size'],
-    print
-
-    user_agent = 'Mozilla/5.0 TencentPlayerVod_1.1.91 tencent_-%s-%s' % (vid, fmt_id)
-
-    fns = os.path.splitext(filename)
-
-    for clip in clips:
-        fn = '%s.%d%s' % (fns[0], clip['idx'], fns[1])
-
-        params = {
-            'vid': vid,
-            'otype': 'xml',
-            'platform': PLAYER_PLATFORM,
-            'format': fmt_id,
-            'charge': 0,
-            'ran': random.random(),
-            'filename': fn,
-            'vt': vt,
-            'appver': PLAYER_VERSION,
-            'cKey': load_key(browser),
-            'encryptVer': KLIB_VERSION,
-        }
-        form = urllib.urlencode(params)
-        #print form
-        resp = browser.open('http://vv.video.qq.com/getvkey', data=form)
-        vkey_body = resp.read()
-        #print vkey_body
-        tree = etree.fromstring(vkey_body)
-
-        vkey = tree.xpath('/root/key/text()')[0]
-        level = tree.xpath('/root/level/text()')[0]
-        sp = tree.xpath('/root/sp/text()')[0]
-
-        clip_size = clip['size']
-        clip_url = '%s%s' % (cdn_url, fn)
-        clip_file = os.path.join(target_dir, fn)
-
-        print 'Clip %s, size %d' % (fn, clip_size), clip_url
-        print 'Save to', clip_file
-        download_clip(clip_file, clip_size, user_agent, clip_url, fmt_name, type_name, fmt_br, sp, vkey, level)
 
 def get_suburl(browser, page_url, target_dir):
     print 'GET', page_url
     page = get_url(browser, page_url)
-    scripts = page.xpath('/html/head/script')
+    scripts = page.xpath('//script')
     for script in scripts:
         if not script.text:
             continue
@@ -228,13 +222,15 @@ def get_suburl(browser, page_url, target_dir):
 #    lang_info = to_dict(match.group(1))
     print video_info['title']
     print 'Length:', video_info['duration']
-    get_videoinfo(browser, target_dir, video_info['vid'])
+    getvinfo(target_dir, page_url, video_info['vid'])
+
 
 def txsp(page_url, target_dir):
     browser = Browser()
     browser.set_handle_robots(False)
     browser.addheaders = [('User-Agent', USER_AGENT)]
     get_suburl(browser, page_url, target_dir)
+
 
 if __name__ == '__main__':
     page_url = sys.argv[1]
